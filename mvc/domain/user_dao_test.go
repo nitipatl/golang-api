44004a014b24cdb489d78cc10c8df037d2a169fc
Nitipat L <nitipat@igeargeek.com>
2021-04-27 11:52:33 +0700

    [Edited] add bench

---
+++ mvc/domain/user_dao_test.go
@@ -0,0 +1,41 @@
+package domain
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUserNoUserFound(t *testing.T) {
+	user, err := GetUser(0)
+
+	assert.Nil(t, user, "we were not expecting a user with id = 0")
+
+	assert.NotNil(t, err, "we were expecting an eror with id = 0")
+
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode, "we were expecting 404 when user id is not found")
+
+	// if user != nil {
+	// 	t.Error("we were not expecting a user with id = 0")
+	// }
+
+	// if err == nil {
+	// 	t.Error("we were expecting an eror with id = 0")
+	// }
+
+	// if err.StatusCode != http.StatusNotFound {
+	// 	t.Error("we were expecting 404 when user id is not found")
+	// }
+}
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(123)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.EqualValues(t, 123, user.Id)
+	assert.EqualValues(t, "Nitipat", user.FirstName)
+	assert.EqualValues(t, "L", user.LastName)
+	assert.EqualValues(t, "iamsvz@gmail.com", user.Email)
+}
---
+++ mvc/utils/sort_util_test.go
@@ -0,0 +1,71 @@
+package utils
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBubbleSortWorstCase(t *testing.T) {
+	els := []int{6, 5, 4, 3, 2, 1}
+	actcual := BubbleSort(els)
+
+	assert.NotNil(t, actcual)
+
+	expected := []int{1, 2, 3, 4, 5, 6}
+	assert.Equal(t, expected, actcual)
+}
+
+func TestBubbleSortBestCase(t *testing.T) {
+	els := []int{1, 2, 3, 4, 5, 6, 7}
+	actcual := BubbleSort(els)
+
+	assert.NotNil(t, actcual)
+
+	expected := []int{1, 2, 3, 4, 5, 6, 7}
+	assert.Equal(t, expected, actcual)
+}
+
+func getElements(n int) []int {
+	result := make([]int, n)
+	i := 0
+	for j := n - 1; j >= 0; j-- {
+		result[i] = j
+		i++
+	}
+	return result
+}
+
+func TestGetElements10(t *testing.T) {
+	els := getElements(10)
+	assert.Equal(t, []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}, els)
+}
+
+func BenchmarkBubbleSort10(b *testing.B) {
+	els := getElements(10)
+	for i := 0; i < b.N; i++ {
+		BubbleSort(els)
+	}
+}
+
+func BenchmarkBubbleSort10000(b *testing.B) {
+	els := getElements(10000)
+	for i := 0; i < b.N; i++ {
+		BubbleSort(els)
+	}
+}
+
+func BenchmarkNativeSort10(b *testing.B) {
+	els := getElements(10)
+	for i := 0; i < b.N; i++ {
+		sort.Ints(els)
+	}
+}
+
+func BenchmarkNativeSort10000(b *testing.B) {
+	els := getElements(10000)
+	for i := 0; i < b.N; i++ {
+		sort.Ints(els)
+	}
+}
